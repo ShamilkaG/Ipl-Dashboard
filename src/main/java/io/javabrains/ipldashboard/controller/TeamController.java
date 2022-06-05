7eeee2d9e1910269bf7109cdf62492576aa1579b
shamilka <sham.gurusinghe@gmail.com>
2022-06-05 15:55:28 +0530

    React App with Team Page

--- src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
+++ src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -2,6 +2,7 @@ package io.javabrains.ipldashboard.controller;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,27 +12,27 @@ import io.javabrains.ipldashboard.repository.MatchRepository;
 import io.javabrains.ipldashboard.repository.TeamRepository;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
     private MatchRepository matchRepository;
-    
 
-    public TeamController(TeamRepository teamRepository,MatchRepository matchRepository) {
+    public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
         this.teamRepository = teamRepository;
         this.matchRepository = matchRepository;
     }
 
-
     @GetMapping("/team/{teamName}")
-    public Team getTeam(@PathVariable String teamName){
+    public Team getTeam(@PathVariable String teamName) {
         Team team = this.teamRepository.findByTeamName(teamName);
 
         // Pageable pageable = PageRequest.of(0, 4);
-        // team.setMatches(this.matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName,pageable));
+        // team.setMatches(this.matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName,
+        // teamName,pageable));
         team.setMatches(this.matchRepository.findLatestMatchesbyTeam(teamName, 4));
 
         return team;
     }
-    
+
 }
