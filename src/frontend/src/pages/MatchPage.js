14b7e5b67a384a6e92ad25569383145aff63d1f3
shamilka <sham.gurusinghe@gmail.com>
2022-06-07 11:59:24 +0530

    Match Page Year selector and Home Page

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,32 +1,39 @@
-import {React,useEffect,useState} from 'react'
-import { useParams } from 'react-router-dom'
-import { MatchDetailCard } from '../components/MatchDetailCard'
-import { MatchSmallCard } from '../components/MatchSmallCard'
+import { React, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { MatchSmallCard } from "../components/MatchSmallCard";
+import { YearSelector } from "../components/YearSelector";
+
+import "./MatchPage.scss";
 
 export const MatchPage = () => {
-    const[matches, setMatches] = useState([]);
-    const {teamName ,year} = useParams();
-    // const teamName = "Delhi Capitals";
-    useEffect(
-        () => {
-            const fetchMatches = async () => {
-                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
-                const data = await response.json();
-                console.log(data);
-                setMatches (data);
-            };
-            fetchMatches();
-        },[]
-    )
+  const [matches, setMatches] = useState([]);
+  const { teamName, year } = useParams();
+  // const teamName = "Delhi Capitals";
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch(
+        `http://localhost:8080/team/${teamName}/matches?year=${year}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setMatches(data);
+    };
+    fetchMatches();
+  }, [teamName, year]);
 
   return (
-    <div className='MatchPage'>
-      <h1>Match Page</h1>
-      {
-          matches.map(match => <MatchDetailCard teamName={teamName} match={match}/>)
-      }
+    <div className="MatchPage">
+      <div className="year-selector">
+        <h3>Select Year</h3>
+        <YearSelector teamName = {teamName}/>
+      </div>
+      <div>
+      <h1 className="page-heading">{teamName} Matches in {year}</h1>
+      {matches.map((match) => (
+        <MatchDetailCard teamName={teamName} match={match} />
+      ))}
+      </div>
     </div>
-  )
-}
-
-
+  );
+};
